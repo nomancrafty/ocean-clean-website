6c5dd5137d6209b00fddc246cf32a6ce8f774fb6
SHAJAR5110 <154994647+SHAJAR5110@users.noreply.github.com>
2026-08-27 01:11:25 +0500

    first commit

--- src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-export function Container({
-  className = "",
-  children,
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) {
-  return <div className={`mx-auto w-full max-w-[1220px] px-5 sm:px-8 ${className}`}>{children}</div>;
-}
